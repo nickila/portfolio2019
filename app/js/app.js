6a63651c8ce721fef988ba115f2f10418a9ee13d
Brian Nickila <brian.nickila@gmail.com>
2019-02-07 23:53:37 -0600

    Updated portfolio with scraper site.

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,3 @@
-$(document).ready(function() {
-    
-    $("#about").on("click", function(event) {
-        event.preventDefault();
-        console.log("about");
-        $("#about-section").slideToggle(900);
-        $("#contact").parent().slideToggle();
-        $("#portfolio").parent().slideToggle();
-        
-        
-    });
-    $("#portfolio").on("click", function(event) {
-        event.preventDefault();
-        console.log("portfolio");
-    });
-    $("#contact").on("click", function(event) {
-        event.preventDefault();
-        console.log("contact");
-    });
 
 
 
@@ -27,4 +8,4 @@ $(document).ready(function() {
 
 
 
-})
+
